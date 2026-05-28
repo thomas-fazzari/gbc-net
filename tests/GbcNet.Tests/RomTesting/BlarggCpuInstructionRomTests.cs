a332783a4f5710b0f3582eb69d7088d837278b04
thomas-fazzari <thomas.fazzari@pm.me>
2026-05-28 21:37:57 +0200

    Run all Blargg CPU instruction ROM tests

--- tests/GbcNet.Tests/RomTesting/BlarggCpuInstructionRomTests.cs
+++ tests/GbcNet.Tests/RomTesting/BlarggCpuInstructionRomTests.cs
@@ -5,14 +5,24 @@ namespace GbcNet.Tests.RomTesting;
 [Collection<RomTestingGroup>]
 public sealed class BlarggCpuInstructionRomTests
 {
-    private const int MaxMachineCycles = 5_000_000;
+    private const string RomDirectory = "RomTesting/Resources/Blargg/cpu_instrs/individual";
+    private const int MaxMachineCycles = 20_000_000;
 
-    [Fact]
-    public void SpecialInstructionsPass()
+    [Theory]
+    [InlineData("01-special.gb")]
+    [InlineData("02-interrupts.gb")]
+    [InlineData("03-op sp,hl.gb")]
+    [InlineData("04-op r,imm.gb")]
+    [InlineData("05-op rp.gb")]
+    [InlineData("06-ld r,r.gb")]
+    [InlineData("07-jr,jp,call,ret,rst.gb")]
+    [InlineData("08-misc instrs.gb")]
+    [InlineData("09-op r,r.gb")]
+    [InlineData("10-bit ops.gb")]
+    [InlineData("11-op a,(hl).gb")]
+    public void CpuInstructionRomPasses(string fileName)
     {
-        byte[] rom = File.ReadAllBytes(
-            "RomTesting/Resources/Blargg/cpu_instrs/individual/01-special.gb"
-        );
+        byte[] rom = File.ReadAllBytes(Path.Combine(RomDirectory, fileName));
 
         RomTestResult result = RomTestRunner.Run(rom, MaxMachineCycles);
 
