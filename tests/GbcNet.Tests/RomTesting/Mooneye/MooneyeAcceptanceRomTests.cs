9c1306a9032f184b82b16a0cc25d7a43e3c1fda0
thomas-fazzari <thomas.fazzari@pm.me>
2026-05-31 10:50:23 +0200

    Expand Mooneye CPU timing coverage

--- tests/GbcNet.Tests/RomTesting/Mooneye/MooneyeAcceptanceRomTests.cs
+++ tests/GbcNet.Tests/RomTesting/Mooneye/MooneyeAcceptanceRomTests.cs
@@ -9,16 +9,29 @@ public sealed class MooneyeAcceptanceRomTests
     private const int MaxMachineCycles = 20_000_000;
 
     [Theory]
+    [InlineData("add_sp_e_timing.gb")]
     [InlineData("bits/reg_f.gb")]
+    [InlineData("call_cc_timing.gb")]
+    [InlineData("call_cc_timing2.gb")]
+    [InlineData("call_timing.gb")]
+    [InlineData("call_timing2.gb")]
     [InlineData("ei_sequence.gb")]
     [InlineData("ei_timing.gb")]
     [InlineData("halt_ime0_ei.gb")]
     [InlineData("if_ie_registers.gb")]
     [InlineData("interrupts/ie_push.gb")]
     [InlineData("instr/daa.gb")]
+    [InlineData("jp_cc_timing.gb")]
+    [InlineData("jp_timing.gb")]
+    [InlineData("ld_hl_sp_e_timing.gb")]
+    [InlineData("pop_timing.gb")]
+    [InlineData("push_timing.gb")]
     [InlineData("rapid_di_ei.gb")]
+    [InlineData("ret_cc_timing.gb")]
+    [InlineData("ret_timing.gb")]
     [InlineData("reti_intr_timing.gb")]
     [InlineData("reti_timing.gb")]
+    [InlineData("rst_timing.gb")]
     public void AcceptanceRomPasses(string relativePath)
     {
         byte[] rom = File.ReadAllBytes(Path.Combine(RomDirectory, relativePath));
