1a9945773ef00ec6a61798cf99e17749d0e77997
thomas-fazzari <122701528+thomas-fazzari@users.noreply.github.com>
2026-08-10 23:51:45 +0200

    Keep configuration sections non-null

--- emulator/src/GbcNet.App/Configuration/AppConfig.cs
+++ emulator/src/GbcNet.App/Configuration/AppConfig.cs
@@ -1,6 +1,7 @@
 // Copyright (C) 2026 thomas-fazzari
 // SPDX-License-Identifier: GPL-3.0-only
 
+using System.Diagnostics.CodeAnalysis;
 using GbcNet.App.Configuration.Sections.Audio;
 using GbcNet.App.Configuration.Sections.BootRom;
 using GbcNet.App.Configuration.Sections.Emulation;
@@ -11,12 +12,28 @@ namespace GbcNet.App.Configuration;
 
 internal sealed class AppConfig
 {
-    public InputConfig Input { get; set; } = AppConfigurationFile.CreateDefaultInputConfig();
+    [AllowNull]
+    public InputConfig Input
+    {
+        get;
+        set => field = value ?? AppConfigurationFile.CreateDefaultInputConfig();
+    } = AppConfigurationFile.CreateDefaultInputConfig();
 
-    public EmulationConfig Emulation { get; set; } = new();
+    [AllowNull]
+    public EmulationConfig Emulation
+    {
+        get;
+        set => field = value ?? new EmulationConfig();
+    } = new();
 
     public AudioConfig Audio { get; set; } = new();
-    public LibraryConfig Library { get; set; } = new();
+
+    [AllowNull]
+    public LibraryConfig Library
+    {
+        get;
+        set => field = value ?? new LibraryConfig();
+    } = new();
 
     public BootRomConfig BootRoms { get; set; }
 }
